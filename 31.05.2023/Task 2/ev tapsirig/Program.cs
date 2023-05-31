9a04d61e663dd2e6debe0b6ac4f2ebd1f5876269
Najafli Najaf <najafnn@code.local>
2023-05-31 13:54:09 +0400

    f

---
+++ 31.05.2023/Task 2/ev tapsirig/Program.cs
@@ -0,0 +1,49 @@
+﻿using ev_tapsirig;
+
+namespace elebele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName, groupNo;
+            int age;
+
+            while (true)
+            {
+                Console.Write("\nAdinizi ve Soyadinizi daxil edin: ");
+                fullName = Console.ReadLine();            
+
+                Console.Write("\nQrup nomrenizi daxil edin: ");
+                groupNo = Console.ReadLine();
+
+                if (!Student.CheckGroupNo(groupNo))
+                {
+                    Console.WriteLine("\nZehmet olmasa qrup nomrenizi duzgun daxil edin. meselucun (P216)");
+                    continue;
+                }
+
+                Console.Write("Yasini daxil et: ");
+                if (!int.TryParse(Console.ReadLine(), out age))
+                {
+                    Console.WriteLine("\nYasi duzgun daxil etmediz xahis olunur duzgun daxil eidn.");
+                    continue;
+                }
+
+                break;
+            }
+            Student student = new Student
+            {
+                FullName = fullName,
+                GroupNo = groupNo,
+                Age = age
+            };
+            Console.WriteLine("Sagird ugurla qydiyattan kecdi: \n *****************\n");
+            Console.WriteLine($"Full Name: {student.FullName}");
+            Console.WriteLine($"Group Nomresi: {student.GroupNo}");
+            Console.WriteLine($"Yasi: {student.Age}");
+        }
+    }
+}    
+    
+
---
+++ 31.05.2023/Task 2/ev tapsirig/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ev_tapsirig
+{
+    public class Student
+    {
+        public string FullName { get; set; }
+        public string GroupNo { get; set; }
+        public int Age { get; set; }
+
+        public static bool CheckGroupNo(string groupNo)
+        {
+            if (groupNo.Length == 4 && char.IsUpper(groupNo[0]) && char.IsDigit(groupNo[1]) && char.IsDigit(groupNo[2]) && char.IsDigit(groupNo[3])) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        
+        }
+    }
+}
